3ed4c8fb3fad3113aca06b31dfd624c4181e9a8a
Ryan Buegtt <ryanburgett615@gmail.com>
2020-12-14 22:21:12 -0600

    Adding exit logic if scale is below specified weight

--- scale/scale.py
+++ scale/scale.py
@@ -1,29 +1,35 @@
 #! /usr/bin/python2
 
-import time
 import sys
-import requests
+import time
 
-EMULATE_HX711=False
+EMULATE_HX711 = False
 
 referenceUnit = 533
 
 URL = "http://127.0.0.1:5000/"
 
+MIN_WEIGHT = 50
+
 if not EMULATE_HX711:
     import RPi.GPIO as GPIO
     from hx711 import HX711
 else:
-    from emulated_hx711 import HX711
+    # from emulated_hx711 import HX711
+    print("Error")
+
+
+
 
 def cleanAndExit():
     print("Cleaning...")
 
     if not EMULATE_HX711:
         GPIO.cleanup()
-        
+
     print("Bye!")
     sys.exit()
+
 
 hx = HX711(5, 6)
 
@@ -39,25 +45,35 @@ hx.tare()
 print("Tare done! Add weight now...")
 
 weights = [hx.get_weight(5)]
+min_count = 0
 
 while True:
     try:
         # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
 
-        if len(weights) >= 20:
+        if len(weights) >= 10:
             weights.pop(0)
 
         weights.append(hx.get_weight(5))
 
         average = sum(weights) / len(weights)
 
-        print(weights)
-        print("Lenght: " + str(len(weights)))
-        print(average)
+        print("Current weights: " + str(weights))
+        print("Length: " + str(len(weights)))
+        print("Average: " + str(average))
 
         hx.power_down()
         hx.power_up()
         time.sleep(0.1)
+
+        if average < MIN_WEIGHT:
+            min_count += 1
+
+            if min_count > 10:
+                print("Scale is below designated weight: Processing Order...")
+                cleanAndExit()
+        else:
+            min_count = 0
 
     except (KeyboardInterrupt, SystemExit):
         cleanAndExit()
